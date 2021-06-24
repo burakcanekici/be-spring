a52a508975da97ef580e81bb5fb122ee3f84b720
Burakcan Ekici <bcekici11@gmail.com>
2021-06-24 19:02:16 +0300

    Delete couchbase-demo directory

--- couchbase-demo/src/main/java/com/be/couchbasedemo/repository/couchbase/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.be.couchbasedemo.repository.couchbase;
-
-
-import com.be.couchbasedemo.domain.couchbase.Company;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CompanyRepository extends CrudRepository<Company, String> {
-
-}
--- couchbase-demo/src/main/java/com/be/couchbasedemo/repository/couchbase/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.be.couchbasedemo.repository.couchbase;
-
-import com.be.couchbasedemo.domain.couchbase.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, String> {
-
-}
