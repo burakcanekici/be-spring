0a5b24fa268a68519ead62ed8316c8343f311b39
Burakcan Ekici <bcekici11@gmail.com>
2021-06-24 19:02:57 +0300

    Delete couchbase-sdk-demo directory

--- couchbase-sdk-demo/src/main/java/com/be/couchbasesdkdemo/repository/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.be.couchbasesdkdemo.repository;
-
-import com.be.couchbasesdkdemo.domain.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface CompanyRepository extends JpaRepository<Company, Long>, JpaSpecificationExecutor<Company> {
-}
