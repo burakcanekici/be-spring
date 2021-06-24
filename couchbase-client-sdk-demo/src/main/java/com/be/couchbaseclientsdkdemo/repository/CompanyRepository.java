91fbb8ce514fac04f8ce25f4b5580d9140600db7
Burakcan Ekici <bcekici11@gmail.com>
2021-06-24 19:12:18 +0300

    Delete couchbase-client-sdk-demo directory

--- couchbase-client-sdk-demo/src/main/java/com/be/couchbaseclientsdkdemo/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.be.couchbaseclientsdkdemo.repository;
-
-import com.be.couchbaseclientsdkdemo.model.entity.Company;
-import org.springframework.data.couchbase.repository.CouchbaseRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CompanyRepository extends CrudRepository<Company, String> {
-
-}
