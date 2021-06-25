96da9e4e132f45192130d8cbbffbfcfeaa44be92
Burakcan Ekici <bcekici11@gmail.com>
2021-06-25 11:59:19 +0300

    Delete couchbase-sdk-demo directory

--- couchbase-sdk-demo/src/main/java/com/be/couchbasesdkdemo/repository/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.be.couchbasesdkdemo.repository;
-
-import com.be.couchbasesdkdemo.model.entity.Company;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CompanyRepository extends CrudRepository<Company, String> {
-
-}
--- couchbase-sdk-demo/src/main/java/com/be/couchbasesdkdemo/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.be.couchbasesdkdemo.repository;
-
-import com.be.couchbasesdkdemo.model.entity.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, String> {
-
-    void deleteEmployeeByName(String name);
-}
